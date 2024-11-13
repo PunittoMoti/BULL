acd115139459d50558c7e4b9c777eb442327451a
Rakki_Naduna <hidetomoti@gmail.com>
2024-11-13 23:48:03 +0900

    闘牛リセット実装

--- Assets/Source/CreateBULL.cs
+++ Assets/Source/CreateBULL.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreateBULL : MonoBehaviour
 {
@@ -36,6 +37,7 @@ public class CreateBULL : MonoBehaviour
             SpawnBull(mBulls[mSpawnCount]);
             mSpawnCount += 1;
         }
+
     }
 
     //ì¨ãçê∂ê¨
@@ -51,6 +53,13 @@ public class CreateBULL : MonoBehaviour
                 break;
         }
     }
+
+    void ResetBull()
+    {
+        mSpawnCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 
 }
 
